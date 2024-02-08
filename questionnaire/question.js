ebacf564f1bce10735ffeabede7be971e39ee3e5
kat1eba3 <baek683@newschool.edu>
2024-02-08 09:24:11 -0500

    Initial commit

---
+++ questionnaire/question.js
@@ -0,0 +1,9 @@
+document.getElementById('playButton').addEventListener('click', function () {
+    // Replace 'YOUR_VIDEO_ID' with the actual YouTube video ID
+    var videoId = 'xvFZjo5PgG0';
+
+    // Open the YouTube video in a new tab
+    window.open('https://www.youtube.com/watch?v=' + videoId, '_blank');
+  });
+
+  console.log("In freshmen year of high school I broke my leg in skyzone playing dodgeball against kids");
